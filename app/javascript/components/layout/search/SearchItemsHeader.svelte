61e3a25501cf4f73959197341af4d1d29fa2bd9d
Bluup-Aomine <vincent-capek@laposte.net>
2020-11-17 15:19:20 +0100

    Add icon search for search bar

--- app/javascript/components/layout/search/SearchItemsHeader.svelte
+++ app/javascript/components/layout/search/SearchItemsHeader.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <form action="#" class="form-inline my-2 mt-lg-0" id="search-header">
+    <i class="fa fa-search"></i>
     <input aria-label="Search" bind:value={value} class="form-control mr-sm-2"
            on:input|preventDefault
            placeholder="Search your posts, categories..." type="search">
