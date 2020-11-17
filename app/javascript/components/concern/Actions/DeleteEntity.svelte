10b7f31e199bb822f99c824c94dfe7560913e906
Bluup-Aomine <vincent-capek@laposte.net>
2020-11-17 13:50:28 +0100

    Fix bug position button delete on table entity

--- app/javascript/components/concern/Actions/DeleteEntity.svelte
+++ app/javascript/components/concern/Actions/DeleteEntity.svelte
@@ -3,7 +3,7 @@
     export let csrfValue = ''
 </script>
 
-<form action={uriAction} method="POST">
+<form action={uriAction} method="POST" class="d-inline">
     <input type="hidden" name="_method" value="delete">
     <input type="hidden" name="authenticity_token" value={csrfValue}>
     <button type="submit" class="btn btn-danger">Delete</button>
